2fedb1dcb46c498d68f4457f15981388ea0d571c
axiomtutor <axiomtutor@protonmail.com>
2023-09-05 22:29:21 -0400

    id not found in p1

--- quartobjspyscript/vite-project/src/p1.ts
+++ quartobjspyscript/vite-project/src/p1.ts
@@ -1,7 +1,10 @@
-import { init } from 'babylon-helper-lib';
+import { init, twodgrid } from 'babylon-helper-lib';
 
 export const p1 = function() {
 
-const c1bag = init("#c1");
+const c1bag = init('#c1');
+const c1scene = c1bag.scene;
+
+twodgrid(c1scene);
 
 };
