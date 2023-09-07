22822a2aef3caa5aa55f1652d416b437b5160452
axiomtutor <axiomtutor@protonmail.com>
2023-09-07 14:14:53 -0400

    tried window. stuff, still doesn't work

--- quartobjspyscript/vite-project/src/main.ts
+++ quartobjspyscript/vite-project/src/main.ts
@@ -1,5 +1,13 @@
-import { index } from './index.ts';
-
 export { index } from './index.ts';
 export { p1 } from './p1.ts';
+import { index } from './index.ts';
+import { p1 } from './p1.ts';
 
+declare global {
+    interface Window {
+        index: any;
+        p1: any;
+    }
+}
+window.index = index;
+window.p1 = p1;
